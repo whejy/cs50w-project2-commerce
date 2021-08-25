11cfb4aa4c14675dafc6c0ce337eccf747d80292
whejy <john.wheldale@outlook.com>
2021-08-25 15:15:01 +0930

    included js for listing page

--- auctions/static/auctions/layout.js
+++ auctions/static/auctions/layout.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const bid = document.querySelector('#id_start_bid')
+    var bid = document.querySelector('#id_start_bid')
 
-    // Adjust starting bid input to display 2 decimal places
+    // Adjust bid input on l'isting' page to display 2 decimal places
+    // and to round to nearest 0.5
+    if (!bid) {
+        var bid = document.querySelector('#id_bid')
+        if (bid) {
+            if (bid.value.length <= 2) {
+                bid.defaultValue = bid.value + '.00'
+            } else if (bid.value.length >= 3) {
+                bid.defaultValue = bid.value + '0'
+            }   
+        }
+    }
+
+    // Adjust starting bid input on 'new' page to display 2 decimal places
     // and to round to nearest 0.5
     if (bid) {
         bid.onchange = () => {
             if (bid.value % 0.5 > 0) {
                 const roundBid = Math.round(bid.value*2)/2
-                bid.value = roundBid;
+                bid.value = roundBid
             }
             if (bid.value.length <= 2) {
                 bid.value = bid.value + '.00'
